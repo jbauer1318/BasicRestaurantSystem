3bd3180d3fe0db99099b2ee2228569bd27262dd6
Jamison Bauer <jbauer13@students.towson.edu>
2020-05-27 15:40:18 -0400

    Add files via upload

---
+++ Commands/CMDGetMenu.java
@@ -0,0 +1,16 @@
+package Commands;
+import Aggregator.*;
+
+public class CMDGetMenu {
+    
+    private Aggregator agg;
+    
+    public CMDGetMenu(Aggregator agg){
+        this.agg = agg;
+    }
+    
+    public Object execute(){
+    return new Menu(agg.getMenu()); 
+    }
+    
+}

216345febab54382477d2184630096d6ed778b43
Jamison Bauer <jbauer13@students.towson.edu>
2020-05-27 15:39:17 -0400

    Add files via upload

---
+++ BasicRestaurant/BasicRestaurantSystem.java
@@ -0,0 +1,15 @@
+package BasicRestaurantSystem;
+
+import java.util.Scanner;
+
+public class BasicRestaurantSystem {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    	
+    	
+   }
+}
+	
+
